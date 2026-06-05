e383869d9c35c20930663411f5bea74526965bb7
Kyame Israel <158627591+yeokj@users.noreply.github.com>
2026-06-05 11:10:32 -0400

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
+        
+        unordered_map<char, int> mp;
+
+        for (char c : s) {
+            ++mp[c];
+        }
+
+        for (char c : t) {
+            if (!mp.count(c)) return false;
+            --mp[c];
+        }
+
+        for (const auto &p : mp) {
+            if (p.second >= 1) return false;
+        }
+
+        return true;
+    }
+};
