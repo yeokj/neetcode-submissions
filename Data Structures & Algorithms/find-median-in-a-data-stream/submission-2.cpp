71c7b6acd1ca50cabefeb96193273b450c18edd1
Kyame Israel <158627591+yeokj@users.noreply.github.com>
2026-07-13 05:54:25 -0400

    Add: find-median-in-a-data-stream - submission-2

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-2.cpp
@@ -0,0 +1,27 @@
+class MedianFinder {
+public:
+    priority_queue<int, vector<int>, greater<int>> highStream;
+    priority_queue<int> lowStream;
+
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+        lowStream.push(num);
+        int val = lowStream.top();
+        lowStream.pop();
+        highStream.push(val);
+
+        if (highStream.size() > lowStream.size()) {
+            val = highStream.top();
+            highStream.pop();
+            lowStream.push(val);
+        }
+    }
+    
+    double findMedian() {
+        if (lowStream.size() > highStream.size()) return (double)lowStream.top();
+        return (highStream.top() + lowStream.top()) / 2.0;
+    }
+};
