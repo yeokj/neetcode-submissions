2f0454323135dc3639d76a29b098c938cf65c307
Kyame Israel <158627591+yeokj@users.noreply.github.com>
2026-07-14 09:58:21 -0400

    Add: count-connected-components - submission-0

---
+++ Data Structures & Algorithms/count-connected-components/submission-0.cpp
@@ -0,0 +1,31 @@
+class Solution {
+private:
+    void dfs(unordered_map<int, vector<int>> &adjList, unordered_set<int> &visited, int i, int parent) {
+        if (visited.count(i)) return;
+        visited.insert(i);
+
+        for (const auto &node : adjList[i]) {
+            if (node == parent) continue;
+            dfs(adjList, visited, node, i);
+        }
+    }
+public:
+    int countComponents(int n, vector<vector<int>>& edges) {
+        unordered_map<int, vector<int>> adjList;
+        for (int i = 0; i < edges.size(); ++i) {
+            int u = edges[i][0], v = edges[i][1];
+            adjList[u].push_back(v);
+            adjList[v].push_back(u);
+        }
+        unordered_set<int> visited;
+        int connect = 0;
+
+        for (int i = 0; i < n; ++i) {
+            if (!visited.count(i)) {
+                dfs(adjList, visited, i, -1);
+                ++connect;
+            }
+        }
+        return connect;
+    }
+};
