989fa87c21d2d9d51fdccf7937bd62578b6d5d4d
Kyame Israel <158627591+yeokj@users.noreply.github.com>
2026-07-14 09:53:40 -0400

    Add: valid-tree - submission-3

---
+++ Data Structures & Algorithms/valid-tree/submission-3.cpp
@@ -0,0 +1,33 @@
+class Solution {
+private:
+    bool dfs(unordered_map<int, vector<int>> &adjList, unordered_set<int> &visited, int i, int parent) {
+        if (visited.count(i)) return false;
+        visited.insert(i);
+
+        for (const auto &node : adjList[i]) {
+            if (node == parent) continue;
+            bool noCycle = dfs(adjList, visited, node, i);
+            if (!noCycle) return false;
+        }
+        return true;
+    }
+
+public:
+    bool validTree(int n, vector<vector<int>>& edges) {
+        if (edges.size() != n - 1) return false;
+        
+        unordered_map<int, vector<int>> adjList;
+        for (int i = 0; i < edges.size(); ++i) {
+            int u = edges[i][0], v = edges[i][1];
+            adjList[u].push_back(v);
+            adjList[v].push_back(u);
+        }
+        unordered_set<int> visited;
+        int connections;
+
+        bool noCycle = dfs(adjList, visited, 0, -1);
+        if (noCycle && visited.size() == n) return true;
+
+        return false;
+    }
+};
