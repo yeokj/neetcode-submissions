e617539a7b5abfc49843a7c8278b04d3d75c1520
Kyame Israel <158627591+yeokj@users.noreply.github.com>
2026-07-29 06:02:42 -0400

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashSet<Integer> seen = new HashSet<>();
+
+        for (int num : nums) {
+            if (seen.contains(num)) return true;
+            seen.add(num);
+        }
+        return false;
+    }
+}
