41fa304394003785eaa6d3a48787afd524b62e55
Kyame Israel <158627591+yeokj@users.noreply.github.com>
2026-07-31 02:15:43 -0400

    Add: longest-palindromic-substring - submission-1

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-1.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        string t = "^";
+        for (char c : s) {
+            t += '#';
+            t += c;
+        }
+        t += "#$";
+
+        int r = 0, c = 0, n = t.length();
+        vector<int> p(t.length(), 0);
+
+        for (int i = 1; i < n - 1; ++i) {
+            if (i < r) p[i] = min(r - i, p[2 * c - i]);
+            else p[i] = 0;
+
+            while (t[i + 1 + p[i]] == t[i - 1- p[i]]) ++p[i];
+            if (i + p[i] > r) {
+                c = i;
+                r = i + p[i];
+            }
+        }
+
+        int maxLen = 0, centerIdx = 0;
+        for (int i = 0; i < p.size(); ++i) {
+            if (p[i] > maxLen) {
+                maxLen = p[i];
+                centerIdx = i;
+            }
+        }
+
+        int start = (centerIdx - maxLen) / 2;
+        return s.substr(start, maxLen);
+    }
+};
