6cbdd197d50f3a3e9a66a2be85a20ff4ed63d352
Kyame Israel <158627591+yeokj@users.noreply.github.com>
2026-08-25 01:16:27 -0400

    Add: course-schedule-ii - submission-3

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-3.cpp
@@ -0,0 +1,35 @@
+class Solution {
+private:
+    
+
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> indegree(numCourses, 0);
+        unordered_map<int, vector<int>> adjList(numCourses);
+        for (const auto &edge : prerequisites) {
+            int a = edge[0], b = edge[1];
+            adjList[b].push_back(a);
+            ++indegree[a];
+        }
+        
+        queue<int> q;
+        for (int i = 0; i < numCourses; ++i) {
+            if (indegree[i] == 0) q.push(i);
+        }
+
+        vector<int> results;
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            results.push_back(node);
+
+            for (const auto &neighbor : adjList[node]) {
+                --indegree[neighbor];
+                if (indegree[neighbor] == 0) q.push(neighbor);
+            }
+        }
+        
+        if (results.size() == numCourses) return results;
+        return {};
+    }
+};
